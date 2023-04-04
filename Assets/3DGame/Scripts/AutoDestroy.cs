0c284e5eaef03e1716ef272d5e05599f89d5b7f0
luke9123 <luke9123@gmail.com>
2023-04-04 20:43:21 +0200

    nineEnd

--- Assets/3DGame/Scripts/AutoDestroy.cs
+++ Assets/3DGame/Scripts/AutoDestroy.cs
@@ -1,25 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoDestroy : MonoBehaviour
 {
-    [SerializeField] float distanceFromCreation = 100;
+    [SerializeField] float destroyDistance = 100;
+    [SerializeField] float destroyTime = 100;
 
     Vector3 startPos;
+    //float startTime;
 
     void Start()
     {
         startPos = transform.position;
+        //startTime= Time.time;
+
+        //Destroy(gameObject, destroyTime); //késleltetve
+
+        //DestroySelf();
+
+        Invoke(nameof(DestroySelf), destroyTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         float currentDistance = Vector3.Distance(startPos, transform.position);
-        if(currentDistance > distanceFromCreation)
+        if(currentDistance > destroyDistance)
         {
             Destroy(gameObject);
         }
+
+        /*float age = Time.time - startTime;
+        if(age > destroyTime)
+        {
+            Destroy(gameObject);
+        }*/
+    }
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 }
